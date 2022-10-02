ab7586e6e5afa2b798d341e426b961789c9ad1a7
Wade Chewning <chewningwade@gmail.com>
2022-10-02 22:52:16 +0700

    Added switch statement.  2nd Function not working.

--- notes.js
+++ notes.js
@@ -4,3 +4,33 @@
 // The function should take player selection and computer selection
 // and should return a string if you win/lose
 // 2.5 function should NOT be case sensitive;
+
+
+switch (playerSelection) { 
+    case 'Rock':
+    console.log('you have selected Rock')
+    break;
+    case 'Paper':
+    console.log('you have selected Paper')
+    break;
+    case 'Scissors':
+    console.log('you have selected Scissors')
+    break;
+
+
+
+
+    if (playerSelection == computerChoice) {
+        console.log("You have tied!");
+    } else if (playerSelection == 'Rock' || computerChoice == 'Rock') {
+        console.log('NOPE!');
+    } else {
+
+    } {
+
+    }
+
+
+    do {
+        playerSelection = prompt('Please choose Rock, Paper, or Scissors.')
+    } while (computerChoice.indexOf(playerSelection) === -1);
